b9b8eb395cc9fcfcc148db58ea2c6cd97ccdb001
Ruan Valente <ruanopensuselinux@gmail.com>
2020-12-01 19:31:28 -0300

    feat: search repository

---
+++ src/app/services/repositories.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Repository } from './../models/repository';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RepositoriesService {
+  url = `https://api.github.com`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getRepository(repository: string): Observable<Repository> {
+    return this.httpClient
+      .get<Repository>(`${this.url}/repos/${repository}`);
+  }
+}
