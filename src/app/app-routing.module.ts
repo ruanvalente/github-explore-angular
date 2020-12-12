3deb640f6a3dd438ff3d4241715e672c3e1a3b21
Ruan Valente <ruanopensuselinux@gmail.com>
2020-12-12 19:38:59 -0300

    feat: add repository info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
+import { RepositoryInfoComponent } from './components/repository-info/repository-info.component';
 
 const routes: Routes = [
-  { path: '', component: SearchComponent },
+  { path: '', component: SearchComponent, pathMatch: 'full' },
+  { path: 'repository/:repository', component: RepositoryInfoComponent },
 ];
 
 @NgModule({
--- src/app/services/repositories.service.ts
+++ src/app/services/repositories.service.ts
@@ -1,8 +1,10 @@
-import { environment } from './../../environments/environment';
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from './../../environments/environment';
+
+import { Issue } from './../models/issue';
 import { Repository } from './../models/repository';
 
 @Injectable({
@@ -13,7 +15,11 @@ export class RepositoriesService {
 
   constructor(private httpClient: HttpClient) {}
 
-  searchRepository(repository: string): Observable<Repository[]> {
-    return this.httpClient.get<Repository[]>(`${this.url}/repos/${repository}`);
+  searchRepository(repository: string): Observable<Repository> {
+    return this.httpClient.get<Repository>(`${this.url}/repos/${repository}`);
+  }
+
+  getIssues(param: string): Observable<Issue[]> {
+    return this.httpClient.get<Issue[]>(`${this.url}/repos/${param}/issues`);
   }
 }
